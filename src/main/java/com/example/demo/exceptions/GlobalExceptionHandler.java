fe7e8e7dee6eb8482aa8e201ef783b61b3028d5f
suyash giri <suyashgiri938@gmail.com>
2026-07-08 20:29:26 +0530

    Exception Handling Integrated

---
+++ src/main/java/com/example/demo/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.example.demo.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex){
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ex.getMessage());
+    }
+}
